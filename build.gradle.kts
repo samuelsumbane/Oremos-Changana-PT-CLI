80d1276109c0413dd3c0a5ff86bba488a28fc84e
samuelsumbane <samuelsumbane143@gmail.com>
2026-09-11 17:09:28 +0200

    Improvement: Prepare project to build with Kotlin/Native instead of JVM

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
-    kotlin("jvm") version "2.2.0"
-    application
+    kotlin("multiplatform") version "2.2.0"
+//    application
 }
 
 group = "com.oremoschanganaptcli"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation("com.github.ajalt.clikt:clikt:5.0.3")
+/*dependencies {
+    implementation("com.github.ajalt.clikt:clikt:5.1.0")
     testImplementation(kotlin("test"))
 }
 
@@ -20,8 +20,40 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
-}
+}*/
 
+/*
 application {
     mainClass.set("com.oremoschanganaptcli.MainKt")
+}*/
+
+
+kotlin {
+    linuxX64 {
+        binaries {
+            executable {
+                baseName = "oremos-cli"
+                entryPoint = "com.oremoschanganaptcli.main"
+            }
+        }
+    }
+
+    mingwX64 {
+        binaries {
+            executable {
+                baseName = "oremos-cli"
+                entryPoint = "com.oremoschanganaptcli.main"
+            }
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation("com.github.ajalt.clikt:clikt:5.0.3")
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
 }
